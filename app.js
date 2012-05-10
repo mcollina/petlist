69da4609f6ed04129a25c6452c01ccab8d3afe90
Matteo Collina <matteo.collina@gmail.com>
2012-05-10 11:29:24 +0200

    Added the port handling.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.post('/add_pet', routes.add_pet);
 
-http.createServer(app).listen(3000);
+var port = process.env.PORT || 3000;
+http.createServer(app).listen(port);
 
-console.log("Express server listening on port 3000");
+console.log("Express server listening on port " + port);
